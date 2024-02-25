9bd93b7b83430acfa90e10b6e88353d7771ffaf9
Nicolas Segatto <nicolasssegatto@icloud.com>
2024-02-25 18:53:22 -0300

    clean arch

---
+++ src/infra/http/controllers/create-question.controller.ts
@@ -0,0 +1,38 @@
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { z } from 'zod'
+import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
+import { ZodValidationPipe } from '../pipes/zod-validation'
+import { CurrentUser } from '@/infra/auth/current-user.decorator'
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema)
+
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>
+
+@Controller('/questions')
+@UseGuards(JwtAuthGuard)
+export class CreateQuestionController {
+  constructor(private createQuestion: CreateQuestionUseCase) {}
+
+  @Post()
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const { title, content } = body
+    const userId = user.sub
+
+    await this.createQuestion.execute({
+      title,
+      content,
+      authorId: userId,
+      attachmentsIds: [],
+    })
+  }
+}
